7bd7b226ecdd902ba3ee638f64ab2292afa7df30
Bao Doan <mizuto.discord@gmail.com>
2026-05-12 13:19:32 +0700

    chore: fetch servers to manage

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -11,5 +11,14 @@ export default {
     client.user?.setActivity('/help | discord.gg/kSTM8B86eA', {
       type: ActivityType.Custom
     })
+
+    // fetch all current servers
+    client.guilds.fetch().then(guilds => {
+      guilds.forEach(guild => {
+        guild.fetch().then(fullGuild => {
+          console.log(`Fetched guild: ${fullGuild.name} (${fullGuild.id}) with ${fullGuild.memberCount} members.`);
+        }).catch(console.error);
+      });
+    }).catch(console.error);
   },
 };
