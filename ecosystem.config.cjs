34b47e020ab902282bc002b378acc484cdbb5f7a
Bao Doan <mizuto.discord@gmail.com>
2026-06-06 13:21:42 +0700

    chore: fix pm2 script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'akobot',
-            script: 'dist/index.js',
+            script: 'dist/src/index.js',
             watch: false,
             env: {
                 NODE_ENV: 'production',
